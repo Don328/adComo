64011941a35cc27defd54e3b5d5708b9c7ed91e2
Don <donavan.olsen@gmail.com>
2022-04-01 15:47:53 -0700

    added additional tasks to seed

--- src/core/Data/Seed.cs
+++ src/core/Data/Seed.cs
@@ -26,7 +26,70 @@ namespace adComo.Data
                 Title = "Task 1"
             };
 
+            var opus2 = new Opus
+            {
+                OpusId = 2,
+                Title = "Task 2"
+            };
+
+            var opus3 = new Opus
+            {
+                OpusId = 3,
+                Title = "Task 3"
+            };
+
+            var opus4 = new Opus
+            {
+                OpusId = 4,
+                Title = "Task 4"
+            };            
+            
+            var opus5 = new Opus
+            {
+                OpusId = 5,
+                Title = "Task 5"
+            };            
+            
+            var opus6 = new Opus
+            {
+                OpusId = 6,
+                Title = "Task 6"
+            };
+                        
+            var opus7 = new Opus
+            {
+                OpusId = 7,
+                Title = "Task 7"
+            };
+
+            var opus8 = new Opus
+            {
+                OpusId = 8,
+                Title = "Task 8"
+            };            
+            
+            var opus9 = new Opus
+            {
+                OpusId = 9,
+                Title = "Task 9"
+            };            
+            
+            var opus10 = new Opus
+            {
+                OpusId = 10,
+                Title = "Task 10"
+            };
+
             opera.Add(opus);
+            opera.Add(opus2);
+            opera.Add(opus3);
+            opera.Add(opus4);
+            opera.Add(opus5);
+            opera.Add(opus6);
+            opera.Add(opus7);
+            opera.Add(opus8);
+            opera.Add(opus9);
+            opera.Add(opus10);
 
             return opera;
         }
