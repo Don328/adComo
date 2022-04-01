ab8455cf67c0632fca4a24127ff0ac5c7aa235db
Don <donavan.olsen@gmail.com>
2022-04-01 15:41:11 -0700

    created data seed

---
+++ src/core/Data/Seed.cs
@@ -0,0 +1,34 @@
+﻿using adComo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adComo.Data
+{
+    internal class Seed
+    {
+        private readonly bool isDbSeed;
+
+        internal Seed(bool isDbSeed = false)
+        {
+            this.isDbSeed = isDbSeed;
+        }
+
+        internal List<Opus> SeedOpera()
+        {
+            var opera = new List<Opus>();
+
+            var opus = new Opus
+            {
+                OpusId = 1,
+                Title = "Task 1"
+            };
+
+            opera.Add(opus);
+
+            return opera;
+        }
+    }
+}
--- src/core/Program.cs
+++ src/core/Program.cs
@@ -1,12 +1,20 @@
-﻿namespace adComo
+﻿using adComo.Data;
+
+namespace adComo
 {
     class Program
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello");
             await Task.Delay(1000);
-            Console.WriteLine("World");
+            var seed = new Seed();
+            var opera = seed.SeedOpera();
+            foreach (var opus in opera)
+            {
+                Console.WriteLine( $"Title: {opus.Title}");
+                Console.WriteLine( $"Id: {opus.OpusId}");
+                Console.WriteLine();
+            }
         }
     }
 }
