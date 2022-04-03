f9442b750e6498a9bc3495e693ce6fc055c1732d
Don <donavan.olsen@gmail.com>
2022-04-03 12:12:25 -0700

    Removed commented legacy code

--- src/core/Program.cs
+++ src/core/Program.cs
@@ -17,48 +17,5 @@ namespace adComo
             state = new State();
             Menu.MainMenu();
         }
-
-        //public static void SelectDisplay()
-        //{
-        //    Console.WriteLine(
-        //        "Select Display: [1]New " +
-        //        "[2]Active [3]Pending " +
-        //        "[4]Completed " +
-        //        "[9]All [0]Exit");
-        //    var key = Console.ReadKey().Key;
-        //    state.SelectMenu(key);
-        //    Console.Clear();
-        //    switch(state.SelectedMenu)
-        //    {
-        //        case ConsoleKey.D1:
-        //            NewTasks.Show();
-        //            SelectDisplay();
-        //            break;
-        //        case ConsoleKey.D2:
-        //            ActiveTasks.Show();
-        //            SelectDisplay();
-        //            break;
-        //        case ConsoleKey.D3:
-        //            PendingTasks.Show();
-        //            SelectDisplay();
-        //            break;
-        //        case ConsoleKey.D4:
-        //            CompletedTasks.Show();
-        //            SelectDisplay();
-        //            break;
-        //        case ConsoleKey.D9:
-        //            NewTasks.Show();
-        //            ActiveTasks.Show();
-        //            PendingTasks.Show();
-        //            CompletedTasks.Show();
-        //            SelectDisplay();
-        //            break;
-        //        case ConsoleKey.D0:
-        //            break;
-        //        default:
-        //            SelectDisplay();
-        //            break;
-        //    }
-        //}
     }
 }
