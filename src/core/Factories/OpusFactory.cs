5c46e330dbff3b3c9591d0087c4b595c63cee6a6
Don <donavan.olsen@gmail.com>
2022-04-04 08:58:36 -0700

    Created opus factory

---
+++ src/core/Factories/OpusFactory.cs
@@ -0,0 +1,22 @@
+﻿using adComo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adComo.Factories
+{
+    internal class OpusFactory
+    {
+        internal static Opus CreateOpus(string title)
+        {
+            var opus = new Opus
+            {
+                Title = title
+            };
+
+            return opus;
+        }
+    }
+}
